2bf47d72831ded6bbb003eef3f8da1cae6948f1f
Jefferson John <johnjefferson089@gmail.com>
2023-02-19 12:07:05 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
  *main - Hello, printf
  *
- * return:0
- */
+ *Return: 0
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
-	return (0);
+	printf("with proper grammar, nut the outcome is a piece of art\n")
+		return (0)
 }
